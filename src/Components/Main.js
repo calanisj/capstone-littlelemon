5b415be9f8666b2ddbd3a5a8a0de2292cfb4b2e7
Carlos <carlos.alanis@gmail.com>
2024-03-29 22:47:58 +0100

    New CSS changes for Mobile Devices

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -14,9 +14,7 @@ function Main() {
             <p>We are a family-owned Mediterreanean restaurant, focused on traditional receipies served with a modern twist.</p>
             <button>Reserve a Table</button>
         </div>
-        <div className="hero-image">
-            <img src={RestaurantFood} alt="RestaurantFood"/>
-        </div>
+        <img src={RestaurantFood} alt="RestaurantFood"/>
        </section>
        <section className="specials">
             <div className='specials-title'>
@@ -26,24 +24,36 @@ function Main() {
             <div className="specials-cards">
                 <article className="specials-card">
                     <img src={GreekSalad} alt="GreekSalad"/>
-                    <h3>Greek Salad</h3>
-                    <p className="price">$12.99</p>
-                    <p className="description"> The famous greek salad of crispy lettuce, peppers, olvise, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-                    <p className='order-delivery'>Order a delivery</p>
+                    <div className='card-details'>
+                        <div className='card-title'>
+                            <h3>Greek Salad</h3>
+                            <p className="price">$12.99</p>
+                        </div>
+                        <p className="description"> The famous greek salad of crispy lettuce, peppers, olvise, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                        <p className='order-delivery'>Order a delivery</p>
+                    </div>
                 </article>
                 <article className="specials-card">
                     <img src={Bruschetta} alt='Bruschetta'/>
-                    <h3>Bruschetta</h3>
-                    <p className="price">$5.99</p>
-                    <p className="description">Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
-                    <p className='order-delivery'>Order a delivery</p>
+                    <div className='card-details'>
+                        <div className='card-title'>
+                            <h3>Bruschetta</h3>
+                            <p className="price">$5.99</p>
+                        </div>
+                        <p className="description">Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+                        <p className='order-delivery'>Order a delivery</p>
+                    </div>
                 </article>
                 <article className="specials-card">
                     <img src={LemonDessert} alt='Lemon Dessert'/>
-                    <h3>Lemon Dessert</h3>
-                    <p className="price">$5.00</p>
-                    <p className="description">This comes straight from grandma's recipe book, every last ingredient has been sources and is as authentic as can be imagined</p>
-                    <p className='order-delivery'>Order a delivery</p>
+                    <div className='card-details'>
+                        <div className='card-title'>
+                            <h3>Lemon Dessert</h3>
+                            <p className="price">$5.00</p>
+                        </div>
+                        <p className="description">This comes straight from grandma's recipe book, every last ingredient has been sources and is as authentic as can be imagined</p>
+                        <p className='order-delivery'>Order a delivery</p>
+                    </div>
                 </article>
             </div>
        </section>
